191a6ed254eefa252fe94b08454e549d5974fcf2
ParsalSilwal <077bct027.parsal@sagarmatha.edu.np>
2023-01-17 16:36:24 +0545

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:magh3app/pages/homepage.dart';
 import 'package:magh3app/pages/loginpage.dart';
+import 'package:magh3app/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,8 +28,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: "/home",
       routes: {
         '/': (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
